d5342eff5629d5c6f56055b88dbad1a2479cf9e6
Midnight_Rabbit <midnightrabbit@protonmail.com>
2020-12-10 14:02:43 +0700

    Add quantity and price to python script

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -2,13 +2,14 @@ import ServerBuilder from "./server/server";
 import DatabaseBuilder from "./database/database";
 import BoardgameRepository from './repository/boardgame';
 import TransactionRepository from './repository/transaction';
+import {PORT} from './configuration/configuration';
 
 const RunServer = async () => {
     const [database, closeDatabase]  = await DatabaseBuilder();
     const boardgameRepository = new BoardgameRepository(database);
     const transactionRepository = new TransactionRepository(database);
     const server = ServerBuilder(boardgameRepository, transactionRepository);
-    server.listen(8080, (err, address) => {
+    server.listen(PORT, (err, address) => {
         if (err) {
           console.error(err)
           closeDatabase()
--- data/data_loader.py
+++ data/data_loader.py
@@ -1,17 +1,38 @@
 import pandas as pd 
 import pymongo
 import logging
+import random
+import os
+import sys
+from dotenv import load_dotenv
 
 def CreateConnection():
-    client = pymongo.MongoClient("mongodb://localhost:27017/")
-    database = client["mydatabase"]
-    collection = database["customers"]
+    load_dotenv()
+    MONGO_HOST = os.getenv("MONGO_HOST")
+    if MONGO_HOST is None:
+        MONGO_HOST = "127.0.0.1"
+        
+    MONGO_PORT = os.getenv("MONGO_PORT")
+    if MONGO_PORT is None:
+        MONGO_PORT = "27017"
+
+    MONGO_DATABASE = os.getenv("MONGO_DATABASE")
+
+    if MONGO_DATABASE is None:
+        logging.fatal("Need Database Name")
+        sys.exit()
+
+    uri = "mongodb://{host}:{port}/".format(host=MONGO_HOST, port=MONGO_PORT)
+    print(uri)
+    client = pymongo.MongoClient(uri)
+    database = client[MONGO_DATABASE]
+    collection = database["boardgame"]
     return collection
 
 def CreateDictionary(game_frame) -> dict:
-    logging.info("Inserting boardgame")
+    logging.info("Inserting boardgame {}".format(game_frame["names"]))
     game_dict = {
-        "game_id": game_frame["game_id"],
+        "id": game_frame["game_id"],
         "image_url": game_frame["image_url"],
         "thumb_url": game_frame["thumb_url"],
         "min_players": game_frame["min_players"],
@@ -19,7 +40,9 @@ def CreateDictionary(game_frame) -> dict:
         "name": game_frame["names"],
         "rating": game_frame["geek_rating"],
         "mechanic": game_frame["mechanic"].split(","),
-        "category": game_frame["category"].split(",")
+        "category": game_frame["category"].split(","),
+        "quantity": random.randint(1, 20),
+        "price": random.randint(500, 3000)
     }
     return game_dict
 
@@ -27,13 +50,12 @@ def main():
     logging.basicConfig(level=logging.INFO)
     logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
     logging.info("Starting data loader")
+    collection = CreateConnection()
     boardgames = pd.read_csv('boardgame.csv')  
     boardgames.dropna(subset=['mechanic', 'category'], inplace=True)
     boardgame_data = [CreateDictionary(game) for index, game in boardgames.iterrows()]
-    collection = CreateConnection()
-    print(len(boardgame_data))
+    logging.info("Inserted {} data points".format(len(boardgame_data)))
     game_ids = collection.insert_many(boardgame_data)
-    logging.info(game_ids)
 
 if __name__ == "__main__":
     main()
